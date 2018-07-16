d364ae76eb40d0ca070b068ca1c95c6a684d632a
Pete Wright <pewright77@gmail.com>
2018-07-16 12:18:25 -0400

    Add check for capitalization

--- commit-msg.py
+++ commit-msg.py
@@ -9,6 +9,7 @@ def is_imperative(msg):
     nlp = spacy.load('en_core_web_sm')
     out = nlp(msg)
 
+    #TODO fix the word rework
     if out[0].tag_ != 'VB':
         print('%s is not a simple verb' % out[0].text)
         return False
@@ -28,6 +29,11 @@ def get_commit_msg():
 
 def main():
     msg = get_commit_msg()
+
+    # check for capitalization
+    if msg[0] == msg[0].lower():
+        print('Commit messages must start with a capital letter')
+        sys.exit(1)
 
     if not is_imperative(msg):
         print('Commit message must be in imperative tense.')
