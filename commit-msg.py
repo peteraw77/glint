1e5f10610710f19dd1e5800be6e8bf805acd7c1f
Pete Wright <pewright77@gmail.com>
2018-07-24 14:16:07 -0400

    Remove the set of problem words
    The words themselves are not what causes the problem, how they are used is

--- commit-msg.py
+++ commit-msg.py
@@ -4,8 +4,6 @@ import spacy
 
 # words that should not appear in an imperative sentence
 non_imperative_words = {'i', 'please', 'you'}
-# words handled poorly by spacy
-problem_words = {'rework', 'update', 'fix'}
 
 def is_imperative(msg):
     nlp = spacy.load('en_core_web_sm')
@@ -13,10 +11,8 @@ def is_imperative(msg):
 
     #TODO find a more elegant solution for problem words
     if out[0].tag_ != 'VB':
-        text = out[0].text.lower()
-        if text not in problem_words:
-            print('%s is not a simple verb' % text)
-            return False
+        print('%s is not a simple verb' % out[0].text)
+        return False
 
     # check if there is a subject
     for word in out:
